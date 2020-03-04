42cbaa9ce16639cc445d29585b659f193854ae4f
Ziyi - Desktop <shangyuyu@gmail.com>
2020-03-04 01:17:17 -0500

    Ready for gravity field

---
+++ Project1/SaveData.m
@@ -0,0 +1,19 @@
+% Save data to 'mat' file after running spring simulation
+% The data will then be used by 'GravityField' to create movie of gravity
+% field
+
+data.M = M;
+data.L = L;
+data.V = V;
+data.A = A;
+data.Spring = Spring;
+
+data.tmax = tmax;
+data.clockmax = clockmax;
+
+if storeData
+    data.XHist = XHist;
+    data.YHist = YHist;
+end
+
+save([filename, '.mat'], 'data');
